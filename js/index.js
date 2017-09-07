247479503810722dd837901412191deddfbeca1f
mattbent <mattfranklinbent@gmail.com>
2017-09-07 10:53:35 -0400

    Setting Up

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$("#home-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#home").offset().top
+    }, 2000);
+});
+$("#about-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about-me").offset().top
+    }, 2000);
+});
+$("#resume-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#resume").offset().top
+    }, 2000);
+});
+$("#projects-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#projects").offset().top
+    }, 2000);
+});
+$("#links-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#links").offset().top
+    }, 2000);
+});
