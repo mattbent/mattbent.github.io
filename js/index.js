7d6ec0974502451504d9d423bb928ebc30284f0a
mattbent <mattfranklinbent@gmail.com>
2017-09-07 11:14:54 -0400

    Testing PDF Viewer

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+'use strict';
 $("#home-btn").click(function() {
     $('html, body').animate({
         scrollTop: $("#home").offset().top
@@ -22,4 +23,37 @@ $("#links-btn").click(function() {
     $('html, body').animate({
         scrollTop: $("#links").offset().top
     }, 2000);
+});
+Promise.all([System.import('pdfjs/display/api'),
+             System.import('pdfjs/display/global'),
+             System.import('pdfjs/display/network'),
+             System.resolve('pdfjs/worker_loader')])
+       .then(function (modules) {
+  var api = modules[0], global = modules[1], network = modules[2];
+  api.setPDFNetworkStreamClass(network.PDFNetworkStream);
+
+  // In production, change this to point to the built `pdf.worker.js` file.
+  global.PDFJS.workerSrc = modules[3];
+
+  // Fetch the PDF document from the URL using promises.
+  api.getDocument('Resume9-7-2017.pdf').then(function (pdf) {
+    // Fetch the page.
+    pdf.getPage(1).then(function (page) {
+      var scale = 1.5;
+      var viewport = page.getViewport(scale);
+
+      // Prepare canvas using PDF page dimensions.
+      var canvas = document.getElementById('the-canvas');
+      var context = canvas.getContext('2d');
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
+
+      // Render PDF page into canvas context.
+      var renderContext = {
+        canvasContext: context,
+        viewport: viewport
+      };
+      page.render(renderContext);
+    });
+  });
 });
